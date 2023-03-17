c3b981dc854925bc05f89ba2b8686336be31f154
Suvankar Dash <118100536+suvankardash@users.noreply.github.com>
2023-03-17 19:32:19 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld    //class declaration
-{
-    public static void main(String[] args)     //method/routine declaration
-    {
-        System.out.println("Hello, World");
-    }
-}
