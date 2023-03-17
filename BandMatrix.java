6703414b3aa7c93fd5bbcbbcc210e0c1be573bff
Suvankar Dash <118100536+suvankardash@users.noreply.github.com>
2023-03-17 19:15:01 +0530

    Update BandMatrix.java

--- BandMatrix.java
+++ BandMatrix.java
@@ -1,3 +1,9 @@
+//  Q. Write a program BandMatrix.java that takes two integer command-line
+//     arguments n and width and prints an n-by-n pattern like the ones below, with a zero (0) for each
+//     element whose distance from the main diagonal is strictly more than width, and an asterisk (*)
+//     for each entry that is not, and two spaces between each 0 or *.
+
+
 import java.util.*;                //importing util.* library for taking the input from the user
 public class BandMatrix           //class declaration
 {
@@ -5,9 +11,9 @@ public class BandMatrix           //class declaration
     {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the value of n=");
-        int n=sc.nextInt();
+        int n=sc.nextInt();                               //entering the value of n for size of the matrix.
         System.out.println("enter the value of width=");
-        int width=sc.nextInt();
+        int width=sc.nextInt();                           //entering the value of column size.
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
